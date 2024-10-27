dbee82853d22fc47e2616b6c2dc5f95dffe971af
phuckemz03 <146057737+phuckemz03@users.noreply.github.com>
2024-10-27 16:58:58 +0700

    updateOrderStatus

--- src/main/java/com/toel/dto/seller/request/Request_Category.java
+++ src/main/java/com/toel/dto/seller/request/Request_Category.java
@@ -1,5 +1,12 @@
 package com.toel.dto.seller.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Request_Category {
     Integer id;
     String name;
