704faf2a986d4c07b1948b791d6619fe772cdc48
Thư Trần <tranngocanhthu23@gmail.com>
2024-10-30 01:31:46 +0700

    git update file .java 30/10 1:31

--- src/main/java/com/toel/dto/user/response/Response_Bill_Shop.java
+++ src/main/java/com/toel/dto/user/response/Response_Bill_Shop.java
@@ -1,38 +1,23 @@
 package com.toel.dto.user.response;
 
 import java.util.Date;
+import java.util.List;
 
 import org.mapstruct.Mapper;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Mapper(componentModel = "spring")
-public class Response_Order {
-	Integer orderID;
-	Double orderTotalPrice;
-	Double orderDiscountPrice;
-	Double orderTotalShippingPrice; 
-	Integer orderTotalQuantity;
-	String orderAddress;
-	Integer orderOrderStatusId;
-	Date createdDatetime;
-	Date updatedDatetime;
-	Integer orderChietKhauId;
-	Integer shopId;
-	String shopName;
-	String shopImageURL;
-	Integer productId;
-	String productName;
-	String productImageURL;
-	String productIntroduce;
-	Integer productQuantity;
-	Double productPrice;
-	Double productDiscountPrice;
+public class Response_Bill_Shop {
+	 Integer shopId; 
+	 String shopName; 
+	 String shopAvatar;
+	 List<Response_Bill> bills;
 }
