abeae13961025c71796e2fde1c86da1a7bdc6635
phuckemz03 <146057737+phuckemz03@users.noreply.github.com>
2024-10-30 15:56:48 +0700

    updateEvalueResponsitory

--- src/main/java/com/toel/repository/EvalueRepository.java
+++ src/main/java/com/toel/repository/EvalueRepository.java
@@ -10,7 +10,7 @@ import com.toel.model.Evalue;
 
 public interface EvalueRepository extends JpaRepository<Evalue, Integer> {
 
-    @Query("SELECT e FROM Evalue e WHERE e.product.account.id = ?1")
+    @Query("SELECT e FROM Evalue e WHERE e.product.account.id = ?1 AND e.idParent = 0")
     Page<Evalue> findByAccountId(Integer account_id, Pageable pageable);
 
     @Query(value = "SELECT * FROM evalues e WHERE product_id = :productId AND account_id = :accountId  AND bill_id = :billId LIMIT 1", nativeQuery = true)
