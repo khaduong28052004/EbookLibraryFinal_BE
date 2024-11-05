3641a97e011711a3b3afd38669c28ff0e1ea4569
Thư Trần <tranngocanhthu23@gmail.com>
2024-11-06 02:57:47 +0700

    update bill

--- src/main/java/com/toel/controller/user/ApiBillUser.java
+++ src/main/java/com/toel/controller/user/ApiBillUser.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.toel.dto.user.response.Response_BillDetail_User;
@@ -40,10 +42,16 @@ public class ApiBillUser {
 	private Service_Bill_User service_Bill_User;
 
 	/* Lấy thông tin order */
-	@PostMapping("/read")
+	@GetMapping("/read")
 	public ResponseEntity<Map<String, Object>> getAllOrdersByOrderStatus(
-			@RequestBody Request_Bill_User requestBillDTO) {
+			@RequestParam String orderStatusFind, 
+	        @RequestParam int userId) {
+		
 		System.out.println("TEST 12333");
+		   Request_Bill_User requestBillDTO = new Request_Bill_User();
+		    requestBillDTO.setOrderStatusFind(orderStatusFind);
+		    requestBillDTO.setUserID(userId);
+		    
 		Map<String, Object> response = service_Bill_User.getBills(requestBillDTO);
 		return ResponseEntity.ok(response);
 	}
@@ -111,7 +119,7 @@ public class ApiBillUser {
 	@PostMapping("/testUpdateStatus")
     public ResponseEntity<String> testUpdateStatus() {
         service_Bill_User.updateOrdersAutomatically();
-        return ResponseEntity.ok("Update Status executed successfully");
-    }
+		return ResponseEntity.ok("Update Status executed successfully");
+	}
 
 }
