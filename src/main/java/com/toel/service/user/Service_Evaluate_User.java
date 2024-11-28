8d5469e368f08d8304997d7ae5c703c9699e9a28
Thư Trần <tranngocanhthu23@gmail.com>
2024-11-29 01:46:21 +0700

    delete comment, update service evaluate

--- src/main/java/com/toel/service/user/Service_Evaluate_User.java
+++ src/main/java/com/toel/service/user/Service_Evaluate_User.java
@@ -19,6 +19,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.toel.dto.user.resquest.Request_Evaluate_User;
+import com.toel.exception.AppException;
+import com.toel.exception.ErrorCode;
 import com.toel.model.Account;
 import com.toel.model.BillDetail;
 import com.toel.model.Evalue;
@@ -36,7 +38,6 @@ import jakarta.persistence.EntityNotFoundException;
 @Service
 public class Service_Evaluate_User {
 
-	// Các repository
 	@Autowired
 	private BillDetailRepository billDetailRepository;
 	@Autowired
@@ -52,19 +53,16 @@ public class Service_Evaluate_User {
 
 	public Map<String, Object> saveEvaluate(Request_Evaluate_User requestEvaluateDTO) {
 		Map<String, Object> response = new HashMap<>();
-
 		try {
 			validateEntitiesExist(requestEvaluateDTO);
-
-			Evalue evaluate = createEvaluate(requestEvaluateDTO);
-
+			createEvaluate(requestEvaluateDTO);
 			response.put("message", "Gửi đánh giá thành công");
 			response.put("status", "successfully");
-		} catch (IllegalArgumentException | EntityNotFoundException e) {
+		} catch (IllegalArgumentException e) {
 			response.put("message", e.getMessage());
 			response.put("status", "fail");
 		} catch (Exception e) {
-			response.put("message", "An unexpected error occurred while saving the evaluation.");
+			response.put("message", e.getMessage());
 			response.put("status", "error");
 			e.printStackTrace();
 		}
@@ -80,63 +78,56 @@ public class Service_Evaluate_User {
 		MultipartFile[] images = requestEvaluateDTO.getImages();
 
 		if (star <= 0) {
-			throw new EntityNotFoundException("Vui lòng để lại đánh giá");
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Vui lòng để lại đánh giá");
 		}
 		if (!accountRepository.existsById(accountId)) {
-			throw new EntityNotFoundException("Tài khoản không tồn tại");
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Tài khoản không tồn tại");
 		}
 		if (!productRepository.existsById(productId)) {
-			throw new EntityNotFoundException("Sản phẩm không tồn tại");
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Sản phẩm không tồn tại");
 		}
 		if (!billDetailRepository.existsById(billDetailId)) {
-			throw new EntityNotFoundException("Đơn hàng không tồn tại");
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Đơn hàng không tồn tại");
 		}
 		if (billDetailRepository.billDetailIsExisted(billDetailId, productId, accountId) == 0) {
-			throw new EntityNotFoundException("Kiểm tra lại thông tin cần đánh giá");
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Kiểm tra lại thông tin cần đánh giá");
 		}
 		if (evaluateRepository.isEvaluate(billDetailId, productId, accountId) == 1) {
-			throw new EntityNotFoundException("Sản phẩm đã đánh giá");
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Sản phẩm đã đánh giá");
 		}
 
 		if (images != null) {
 			for (MultipartFile image : images) {
 				try {
 					if (!isValidImageFormat(image)) {
-						throw new IllegalArgumentException("Chỉ chấp nhận các định dạng jpg, jpeg, và png");
+						throw new AppException(ErrorCode.OBJECT_SETUP, "Chỉ chấp nhận các định dạng jpg, jpeg, và png");
 					}
 					checkImageAttributes(image);
 				} catch (IOException e) {
-					throw new IllegalArgumentException("Lỗi khi xử lý ảnh: " + image.getOriginalFilename(), e);
+					throw new AppException(ErrorCode.OBJECT_SETUP, "Lỗi khi xử lý ảnh: " + image.getOriginalFilename(),
+							e);
 				}
 			}
 		}
+
 	}
 
 	private Evalue createEvaluate(Request_Evaluate_User requestEvaluateDTO) {
+		Account account = accountRepository.findById(requestEvaluateDTO.getAccountId()).get();
+		Product product = productRepository.findById(requestEvaluateDTO.getProductId()).get();
+		BillDetail billDetail = billDetailRepository.findById(requestEvaluateDTO.getBillDetailId()).get();
+
 		Evalue evaluate = new Evalue();
 		evaluate.setCreateAt(new Date());
 		evaluate.setIdParent(0);
 		evaluate.setStar(requestEvaluateDTO.getStar());
 		evaluate.setContent(requestEvaluateDTO.getContent());
-
-		for (MultipartFile image : requestEvaluateDTO.getImages()) {
-			System.out.println("image: "+image+"\n");
-		}
-		Account account = accountRepository.findById(requestEvaluateDTO.getAccountId())
-				.orElseThrow(() -> new EntityNotFoundException("Tài khoản không tồn tại"));
 		evaluate.setAccount(account);
-
-		Product product = productRepository.findById(requestEvaluateDTO.getProductId())
-				.orElseThrow(() -> new EntityNotFoundException("Sản phẩm không tồn tại"));
 		evaluate.setProduct(product);
-
-		BillDetail billDetail = billDetailRepository.findById(requestEvaluateDTO.getBillDetailId())
-				.orElseThrow(() -> new EntityNotFoundException("Đơn hàng không tồn tại"));
 		evaluate.setBillDetail(billDetail);
-
 		evaluateRepository.save(evaluate);
 
-		if (requestEvaluateDTO.getImages() != null && requestEvaluateDTO.getImages().length > 0) {
+		if (requestEvaluateDTO.getImages() != null) {
 			List<ImageEvalue> imageEvalues = saveImages(requestEvaluateDTO.getImages(), evaluate);
 			evaluate.setImageEvalues(imageEvalues);
 			evaluateRepository.save(evaluate);
@@ -163,7 +154,7 @@ public class Service_Evaluate_User {
 
 		BufferedImage bufferedImage = ImageIO.read(image.getInputStream());
 		if (bufferedImage == null) {
-			throw new IllegalArgumentException("Không thể đọc ảnh: " + image.getOriginalFilename());
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Không thể đọc ảnh: " + image.getOriginalFilename());
 		}
 
 		int width = bufferedImage.getWidth();
@@ -171,7 +162,7 @@ public class Service_Evaluate_User {
 		long size = image.getSize();
 
 		if (size > 5 * 1024 * 1024) {
-			throw new IllegalArgumentException("Kích thước ảnh không vượt quá 5MB");
+			throw new AppException(ErrorCode.OBJECT_SETUP, "Kích thước ảnh không vượt quá 5MB");
 		}
 
 	}
@@ -184,7 +175,8 @@ public class Service_Evaluate_User {
 				try {
 					String imageFirebaseURL = firebaseUploadImages.uploadFile("evalue", imageFile);
 					if (imageFirebaseURL == null || imageFirebaseURL.isEmpty()) {
-						throw new IOException("Failed to upload image to Firebase: " + imageFile.getOriginalFilename());
+						throw new AppException(ErrorCode.OBJECT_SETUP,
+								"Failed to upload image to Firebase: " + imageFile.getOriginalFilename());
 					}
 
 					ImageEvalue imageEvalue = new ImageEvalue();
@@ -195,7 +187,8 @@ public class Service_Evaluate_User {
 					imageEvalueList.add(imageEvalue);
 				} catch (Exception e) {
 					e.printStackTrace();
-					throw new RuntimeException("Error processing image: " + imageFile.getOriginalFilename(), e);
+					throw new AppException(ErrorCode.OBJECT_SETUP,
+							"Error processing image: " + imageFile.getOriginalFilename(), e);
 				}
 			}
 		}
