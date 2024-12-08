2476ea7a8b267d3dab430c9fc034cc5b4e55a984
khadv28 <khaduong28052004@gmail.com>
2024-12-08 23:13:43 +0700

    suggest

--- src/main/java/com/toel/service/user/Service_SelectAllProductHome.java
+++ src/main/java/com/toel/service/user/Service_SelectAllProductHome.java
@@ -14,8 +14,14 @@ import org.springframework.stereotype.Service;
 
 import com.toel.dto.user.response.Response_Product;
 import com.toel.mapper.user.ProductMaperUser;
+import com.toel.model.Account;
+import com.toel.model.Bill;
+import com.toel.model.BillDetail;
 import com.toel.model.FlashSaleDetail;
 import com.toel.model.Product;
+import com.toel.repository.AccountRepository;
+import com.toel.repository.BillDetailRepository;
+import com.toel.repository.BillRepository;
 import com.toel.repository.ProductRepository;
 
 import lombok.AccessLevel;
@@ -28,13 +34,16 @@ import lombok.experimental.FieldDefaults;
 public class Service_SelectAllProductHome {
 	final ProductRepository productRepo;
 	final ProductMaperUser productMaper;
+	final BillRepository billRepository;
+	final AccountRepository accountRepository;
+	final BillDetailRepository billDetailRepository;
 
 	public Map<String, Object> selectAll(List<FlashSaleDetail> list, Integer idShop, Integer page, Integer size,
 			String sort) {
 
 		List<Integer> idProducts = list.stream().map(p -> p.getProduct().getId()).collect(Collectors.toList());
 		Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
-		Page<Product> pageProducts = productRepo.findAllIdNotIn(idProducts, idShop,pageable);
+		Page<Product> pageProducts = productRepo.findAllIdNotIn(idProducts, idShop, pageable);
 		List<Response_Product> response_Products = new ArrayList<Response_Product>();
 		for (Product product : pageProducts) {
 			if (product.getAccount().getId() != idShop) {
@@ -49,7 +58,13 @@ public class Service_SelectAllProductHome {
 			response.put("error", 1002);
 		}
 		return response;
-
 	}
 
+//	 Gợi ý
+	public List<Response_Product> suggestProduct(Integer id_user) {
+		Account account = accountRepository.findById(id_user).get();
+		List<BillDetail> listBillDetails = billDetailRepository.findAllByUser(id_user);
+		List<Response_Product> listResponse_Products = new ArrayList<Response_Product>();
+		return listResponse_Products;
+	}
 }
